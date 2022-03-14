ac400d35c4879b3ee7f25a71df1ce611b58420fc
AnjaMis <bgp916@myy.haaga-helia.fi>
2022-03-15 00:40:57 +0200

    query parameters validation added

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
-const express = require("express");
-const app = express();
-const mariadb = require("mariadb");
+const express = require('express')
+const app = express()
+const mariadb = require('mariadb')
 const pool = mariadb.createPool({
-  host: "127.0.0.1",
-  port: "3306",
+  host: '127.0.0.1',
+  port: '3306',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectionLimit: 5,
-});
+})
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello world</h1>");
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
+})
 
 // this part gets the URL and parses the parameters from the query string
-app.get("/items", (request, response) => {
+app.get('/items', (request, response) => {
   // printing the query object to console just to see if it's correct
-  console.log(JSON.stringify(request.query));
+  console.log(JSON.stringify(request.query))
 
   // saving query params in variables
-  let uuid = request.query.uuid;
-  let countrycode = request.query.countrycode;
-  let order = request.query.orderID;
+  let uuid = request.query.uuid
+  let countrycode = request.query.countrycode
+  let order = request.query.orderID
 
-  response.send(
-    "UUID is " +
-      uuid +
-      " countrycode is " +
-      countrycode +
-      " order type is " +
-      order
-  );
-});
+  // checking if query parameters are missing
+  if (!uuid || !countrycode || !order) {
+    console.error('Missing query parameters')
+  }
 
-const PORT = 3001;
+  var validatedCountryCode = false
+  var validatedOrderType = false
+
+  //validating country codes with external npm module
+  const lookup = require('country-code-lookup')
+
+  for (var i = 0; i < lookup.countries.length; i++) {
+    if (countrycode == lookup.countries[i].internet) {
+      validatedCountryCode = true
+    }
+  }
+
+  //validating order type (if between 1-10)
+  if (order > 0 && order < 11) {
+    validatedOrderType = true
+  }
+
+  if (validatedCountryCode && validatedOrderType) {
+    response.send(
+      'UUID is ' +
+        uuid +
+        ' countrycode is ' +
+        countrycode +
+        ' order type is ' +
+        order,
+    )
+  } else {
+    console.error('Query parameters invalid')
+  }
+})
+
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+  console.log(`Server running on ${PORT}`)
+})
