637f6980babbb6ec9157415b627320b055d0f0c1
AnjaMis <bgp916@myy.haaga-helia.fi>
2022-03-15 11:50:49 +0200

    query param validation - prettier config fixed

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-const express = require('express')
-const app = express()
-const mariadb = require('mariadb')
+const express = require("express");
+const app = express();
+const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-  host: '127.0.0.1',
-  port: '3306',
+  host: "127.0.0.1",
+  port: "3306",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectionLimit: 5,
-})
+});
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
 
 // this part gets the URL and parses the parameters from the query string
-app.get('/items', (request, response) => {
+app.get("/items", (request, response) => {
   // printing the query object to console just to see if it's correct
-  console.log(JSON.stringify(request.query))
+  console.log(JSON.stringify(request.query));
 
   // saving query params in variables
-  let uuid = request.query.uuid
-  let countrycode = request.query.countrycode
-  let order = request.query.orderID
+  let uuid = request.query.uuid;
+  let countrycode = request.query.countrycode;
+  let order = request.query.orderID;
 
   // checking if query parameters are missing
   if (!uuid || !countrycode || !order) {
-    response.sendStatus(400)
-    return
+    response.sendStatus(400);
+    return;
   }
 
-  var validatedCountryCode = false
-  var validatedOrderType = false
+  var validatedCountryCode = false;
+  var validatedOrderType = false;
 
   //validating country codes with external npm module
-  const lookup = require('country-code-lookup')
+  const lookup = require("country-code-lookup");
 
   for (var i = 0; i < lookup.countries.length; i++) {
     if (countrycode == lookup.countries[i].internet) {
-      validatedCountryCode = true
+      validatedCountryCode = true;
     }
   }
 
   //validating order type (if between 1-10)
   if (order >= 1 && order <= 10) {
-    validatedOrderType = true
+    validatedOrderType = true;
   }
 
   if (validatedCountryCode && validatedOrderType) {
     response.send(
-      'UUID is ' +
+      "UUID is " +
         uuid +
-        ' countrycode is ' +
+        " countrycode is " +
         countrycode +
-        ' order type is ' +
-        order,
-    )
+        " order type is " +
+        order
+    );
   } else {
-    response.sendStatus(400)
+    response.sendStatus(400);
   }
-})
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
+  console.log(`Server running on ${PORT}`);
+});
