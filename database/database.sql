c95aa3823cf95fe2c5cbe3a85700fabb9f76c1e4
WuX <axel.riska@gmail.com>
2022-04-07 09:14:27 +0200

    Updates

--- database/database.sql
+++ database/database.sql
@@ -3,5 +3,7 @@ CREATE TABLE `orders` (
   `uuid` varchar(24) NOT NULL,
   `country_code` varchar(10) NOT NULL,
   `order_type` int(11) NOT NULL,
+  `validated` int(1) NOT NULL DEFAULT 0,
+  `last_changed` date NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
